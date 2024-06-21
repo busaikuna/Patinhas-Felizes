24ffb2054f0b0eb921c13587b9a446d6487cc6ba
Samuel Damasceno Sales <132498977+busaikuna@users.noreply.github.com>
2024-06-21 17:34:11 -0300

    organização para deploy na Vercel

--- static/javascript/styles.js
+++ static/javascript/styles.js
@@ -3,10 +3,10 @@ let like = document.querySelectorAll(".like")
 
 like.forEach(function(button){
     button.addEventListener("click", () =>{
-        if(button.style.backgroundImage == `url("../icons/focusLikeIcon.png")`){
-            button.style.backgroundImage = `url("../icons/likeIcon.png")`
+        if(button.style.backgroundImage == `url("./static/icons/focusLikeIcon.png")`){
+            button.style.backgroundImage = `url("./static/icons/likeIcon.png")`
         }else{
-            button.style.backgroundImage = `url("../icons/focusLikeIcon.png")`
+            button.style.backgroundImage = `url("./static/icons/focusLikeIcon.png")`
         }
     })
 })
